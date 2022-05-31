c50fd717677ff7294f4d5f4abf2d746f3c8e96c2
jan-cibulka <jc@pntr.eu>
2022-05-31 20:17:40 +0300

    Started marketplace tests.

--- backend/test/NftMarketplaceV2.test.ts
+++ backend/test/NftMarketplaceV2.test.ts
@@ -5,55 +5,71 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 const collectionParams = ["Crypto Zombies", "CZ"];
 
-describe("BookLibraryV2", function () {
-  let nftMarketplace: Contract;
+const setGlobals = async () => {
+  const accounts = await ethers.getSigners();
+
+  const NftMarketplaceV2 = await ethers.getContractFactory("NftMarketplaceV2");
+  const nftMarketplaceV2 = await NftMarketplaceV2.deploy();
+  await nftMarketplaceV2.deployed();
+
+  const NftCollection = await ethers.getContractFactory("NftCollection");
+
+  return { nftMarketplaceV2, NftCollection, accounts };
+};
+
+describe("NftMarketplaceV2", function () {
+  let nftMarketplaceV2: Contract;
   let NftCollection: ContractFactory;
+  let nftCollection: Contract;
   let accounts: Array<SignerWithAddress>;
 
   before(async function () {
-    accounts = await ethers.getSigners();
-
-    const NftMarketplace = await ethers.getContractFactory("NftMarketplace");
-    nftMarketplace = await NftMarketplace.deploy();
-    await nftMarketplace.deployed();
-
-    NftCollection = await ethers.getContractFactory("NftCollection");
+    const globals = await setGlobals();
+    nftMarketplaceV2 = globals.nftMarketplaceV2;
+    NftCollection = globals.NftCollection;
+    accounts = globals.accounts;
   });
 
-  it("Should create a collection", async function () {
+  it("Should deploy a collection", async function () {
     const actor = 1;
 
-    const createCollection = await nftMarketplace
-      .connect(accounts[actor])
-      .createCollection(...collectionParams);
-    await createCollection.wait();
+    nftCollection = await NftCollection.connect(accounts[actor]).deploy(
+      ...collectionParams,
+      nftMarketplaceV2.address
+    );
+    await nftCollection.deployed();
 
-    const collection = await nftMarketplace.collections(0);
-    expect(collection.owner).to.equal(accounts[actor].address);
+    await expect(
+      nftMarketplaceV2
+        .connect(accounts[actor])
+        .addCollection(nftCollection.address)
+    )
+      .to.emit(nftMarketplaceV2, "CollectionAdded")
+      .withArgs(accounts[actor].address, nftCollection.address);
   });
 
-  it("Should not mint a token from not-owned collection", async function () {
+  xit("Should not mint a token from not-owned collection", async function () {
     const collectionId = 0;
     const actor = 2;
 
     await expect(
-      nftMarketplace
+      nftMarketplaceV2
         .connect(accounts[actor])
         .mintNftFromCollection(collectionId)
     ).to.be.revertedWith("Collection owner mismatch.");
   });
 
-  it("Should mint a new token", async function () {
+  xit("Should mint a new token", async function () {
     const collectionId = 0;
     const tokenId = 0;
     const actor = 1;
 
-    const mintNftFromCollection = await nftMarketplace
+    const mintNftFromCollection = await nftMarketplaceV2
       .connect(accounts[actor])
       .mintNftFromCollection(collectionId);
     await mintNftFromCollection.wait();
 
-    const collection = await nftMarketplace.collections(collectionId);
+    const collection = await nftMarketplaceV2.collections(collectionId);
     const nftCollection = await NftCollection.attach(
       collection.contractAddress
     );
@@ -63,7 +79,7 @@ describe("BookLibraryV2", function () {
     expect(ownerOfToken).to.equal(accounts[actor].address);
   });
 
-  it("Should not set not-owned token for sale", async function () {
+  xit("Should not set not-owned token for sale", async function () {
     const collectionId = 0;
     const tokenId = 0;
     const price = ethers.utils.parseEther("1");
@@ -71,13 +87,13 @@ describe("BookLibraryV2", function () {
     const actor = 2;
 
     await expect(
-      nftMarketplace
+      nftMarketplaceV2
         .connect(accounts[actor])
         .addMarketItem(collectionId, tokenId, price, { value: fee })
     ).to.be.revertedWith("Token not transferred.");
   });
 
-  it("Should not set zero-price token for sale", async function () {
+  xit("Should not set zero-price token for sale", async function () {
     const collectionId = 0;
     const tokenId = 0;
     const price = ethers.utils.parseEther("0");
@@ -85,13 +101,13 @@ describe("BookLibraryV2", function () {
     const actor = 1;
 
     await expect(
-      nftMarketplace
+      nftMarketplaceV2
         .connect(accounts[actor])
         .addMarketItem(collectionId, tokenId, price, { value: fee })
     ).to.be.revertedWith("Price not set.");
   });
 
-  it("Should set token for sale", async function () {
+  xit("Should set token for sale", async function () {
     const collectionId = 0;
     const tokenId = 0;
     const marketItemId = 0;
@@ -100,7 +116,7 @@ describe("BookLibraryV2", function () {
     const actor = 1;
 
     // Collection contract
-    const collection = await nftMarketplace.collections(collectionId);
+    const collection = await nftMarketplaceV2.collections(collectionId);
     const nftCollection = await NftCollection.attach(
       collection.contractAddress
     );
@@ -108,34 +124,34 @@ describe("BookLibraryV2", function () {
     // Transferring ownership
     const transferFrom = await nftCollection
       .connect(accounts[actor])
-      .transferFrom(accounts[actor].address, nftMarketplace.address, tokenId);
+      .transferFrom(accounts[actor].address, nftMarketplaceV2.address, tokenId);
     await transferFrom.wait();
 
     expect(await nftCollection.ownerOf(tokenId)).to.equal(
-      nftMarketplace.address
+      nftMarketplaceV2.address
     );
 
     // Listing the item
-    const addMarketItem = await nftMarketplace
+    const addMarketItem = await nftMarketplaceV2
       .connect(accounts[actor])
       .addMarketItem(collectionId, tokenId, price, { value: fee });
     await addMarketItem.wait();
 
-    const marketItem = await nftMarketplace.marketItems(marketItemId);
+    const marketItem = await nftMarketplaceV2.marketItems(marketItemId);
     expect(marketItem.collectionId).to.equal(collectionId);
     expect(marketItem.tokenId).to.equal(tokenId);
     expect(marketItem.seller).to.equal(accounts[actor].address);
     expect(marketItem.price).to.equal(price);
   });
 
-  it("Should list market items", async function () {
+  xit("Should list market items", async function () {
     const collectionId = 0;
     const tokenId = 0;
     const marketItemId = 0;
     const price = ethers.utils.parseEther("1");
     const actor = 1;
 
-    const marketItems = await nftMarketplace
+    const marketItems = await nftMarketplaceV2
       .connect(accounts[actor])
       .fetchMarketItems();
     expect(marketItems[marketItemId].collectionId).to.equal(collectionId);
@@ -144,7 +160,7 @@ describe("BookLibraryV2", function () {
     expect(marketItems[marketItemId].price).to.equal(price);
   });
 
-  it("Should sell market item", async function () {
+  xit("Should sell market item", async function () {
     const collectionId = 0;
     const tokenId = 0;
     const marketItemId = 0;
@@ -152,7 +168,7 @@ describe("BookLibraryV2", function () {
     const actor = 2;
 
     const provider = waffle.provider;
-    const marketItemBefore = await nftMarketplace.marketItems(marketItemId);
+    const marketItemBefore = await nftMarketplaceV2.marketItems(marketItemId);
     const balanceSellerBefore = await provider.getBalance(
       marketItemBefore.seller
     );
@@ -160,17 +176,17 @@ describe("BookLibraryV2", function () {
       accounts[actor].address
     );
 
-    const sellMarketItem = await nftMarketplace
+    const sellMarketItem = await nftMarketplaceV2
       .connect(accounts[actor])
       .sellMarketItem(marketItemId, { value: price });
     await sellMarketItem.wait();
 
-    const marketItems = await nftMarketplace
+    const marketItems = await nftMarketplaceV2
       .connect(accounts[actor])
       .fetchMarketItems();
     expect(marketItems.length).to.equal(0);
 
-    const collection = await nftMarketplace.collections(collectionId);
+    const collection = await nftMarketplaceV2.collections(collectionId);
     const nftCollection = await NftCollection.attach(
       collection.contractAddress
     );
@@ -178,7 +194,7 @@ describe("BookLibraryV2", function () {
       accounts[actor].address
     );
 
-    const marketItemAfter = await nftMarketplace.marketItems(marketItemId);
+    const marketItemAfter = await nftMarketplaceV2.marketItems(marketItemId);
     expect(marketItemAfter.buyer).to.equal(accounts[actor].address);
 
     const balanceSellerAfter = await provider.getBalance(
